d73496a3a1c41b0a2844c26cd245aced0926e310
Matheus Gomes <math.s.gomes@gmail.com>
2021-10-23 17:59:00 -0300

    trabalhando com reducer, map e filter para mapear os resultados do JSON vindo da API

--- public/app/app.js
+++ public/app/app.js
@@ -4,5 +4,10 @@ document
 .querySelector('#myButton')
 .onclick = () => fetch('http://localhost:3000/notas')
   .then(handleStatus)
-  .then(notas => console.log(notas))
+  .then(notas => notas.map(notas => notas.itens))
+  .then(itens => itens.filter(item => item.codigo == '2134'))
+  .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+  .then(console.log)
+  // .then(total => console.log(total))
   .catch(console.log)
+
