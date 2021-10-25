fa00059702818f0938f4d28c3d50668571dff965
Matheus Gomes <math.s.gomes@gmail.com>
2021-10-25 11:00:39 -0300

    criado o $flatMap no array helpers para tornar o código mais legível

--- public/app/app.js
+++ public/app/app.js
@@ -1,13 +1,14 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
 
 document
-.querySelector('#myButton')
-.onclick = () => fetch('http://localhost:3000/notas')
+  .querySelector('#myButton')
+  .onclick = () => fetch('http://localhost:3000/notas')
   .then(handleStatus)
-  .then(notas => notas.map(notas => notas.itens))
-  .then(itens => itens.filter(item => item.codigo == '2134'))
-  .then(itens => itens.reduce((total, item) => total + item.valor, 0))
-  .then(console.log)
-  // .then(total => console.log(total))
-  .catch(console.log)
-
+  .then(notas => notas
+      .$flatMap(nota => nota.itens)
+      .filter(item => item.codigo == '2143')
+      .reduce((total, item) =>  total + item.valor, 0)
+  )
+  .then(log) // 590
+  .catch(log)
