c764a9318761aba995d0d6e0f89f6a6af0714d92
Matheus Gomes <math.s.gomes@gmail.com>
2021-10-28 10:55:18 -0300

    aplicação parcial

--- public/app/app.js
+++ public/app/app.js
@@ -2,12 +2,21 @@ import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 
+const ehDivisivel = (divisor, numero) => !(numero % divisor);
+
+// console.log(ehDivisivel(2, 10)); // true
+// console.log(ehDivisivel(2, 5)); // false
+
+const ehDivisivelPorDois = ehDivisivel.bind(null, 2)
+
+console.log(ehDivisivelPorDois(10)); // true
+console.log(ehDivisivelPorDois(5)); // false
 
 
 document
 .querySelector('#myButton')
 .onclick = () => 
   service
-  .sumItens('2143')
+  .sumItems('2143')
   .then(log) // 590
   .catch(log)
--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -2,6 +2,13 @@ import { handleStatus } from '../utils/promise-helpers.js';
 
 const API = 'http://localhost:3000/notas';
 
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
+
+const sumItemsValue = items => items.reduce((total, item) =>  total + item.valor, 0);
+
+
 const sumItems = code => notas => notas
   .$flatMap(nota => nota.itens)
   .filter(item => item.codigo == code)
@@ -14,7 +21,10 @@ export const notasService = {
     return fetch(API).then(handleStatus)
   },
 
-  sumItens(code) {
-    return this.listAll().then(sumItems(code))
+  sumItems(code) {
+    
+    const filterItems = filterItemsByCode.bind(null, code);
+    
+    return this.listAll().then(sumItems(code));
   }
 }
