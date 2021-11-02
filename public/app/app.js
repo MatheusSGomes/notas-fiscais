4ec9eb33c356548025aa4a24fd03c7bfcc6079fd
Matheus Gomes <math.s.gomes@gmail.com>
2021-11-02 11:31:50 -0300

    Limitar as chamadas da API 3x com o operator takeUntil inspirado no RxJS

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,15 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
+
+const operation = takeUntil(3, () => 
+  service
+    .sumItems('2143')
+    .then(log) // 590
+    .catch(log)
+)
 
 document
 .querySelector('#myButton')
-.onclick = () => 
-  service
-  .sumItems('2143')
-  .then(log) // 590
-  .catch(log)
+.onclick = operation;
