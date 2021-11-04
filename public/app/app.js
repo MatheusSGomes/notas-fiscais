645c1dda3209d54b1dd0d7801cc72bc61d319785
Matheus Gomes <math.s.gomes@gmail.com>
2021-11-04 10:04:14 -0300

    debounceTime criado - função que bloqueia os cliques feitos várias vezes seguidas por 500 milissegundos

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,18 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 
-const operation = takeUntil(3, () => 
-  service
-    .sumItems('2143')
-    .then(log) // 590
-    .catch(log)
+const action = debounceTime(500, 
+  takeUntil(3, () => 
+    service
+      .sumItems('2143')
+      .then(log) // 590
+      .catch(log)
+  )
 )
 
+
 document
-.querySelector('#myButton')
-.onclick = operation;
+  .querySelector('#myButton')
+  .onclick = action;
