47043147531b118af8abba2816d3e3180a1ef443
Matheus Gomes <math.s.gomes@gmail.com>
2021-11-09 10:00:07 -0300

    exemplificação da funcionalidade de retry que será usada no programa

--- public/app/app.js
+++ public/app/app.js
@@ -3,6 +3,15 @@ import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe, compose } from './utils/operators.js';
 
+
+const p1 = new Promise((resolve, reject) => setTimeout(() => resolve('Promise 1 terminou'), 3000))
+
+const p2 = new Promise((resolve, reject) => setTimeout(() => reject('Cancelado'), 1000))
+
+Promise.race([p1, p2])
+  .then(console.log)
+  .catch(console.log)
+
 const operations = pipe(
   partialize(takeUntil, 3),
   partialize(debounceTime, 500),
