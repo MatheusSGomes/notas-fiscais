6f4207d2ffe65889d54a2974132053bb02443353
Matheus Gomes <math.s.gomes@gmail.com>
2021-11-09 10:27:38 -0300

    implementando o recurso de retry com timeoutPromise

--- public/app/app.js
+++ public/app/app.js
@@ -1,25 +1,15 @@
-import { handleStatus, log } from './utils/promise-helpers.js';
+import { handleStatus, log, timeoutPromise } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe, compose } from './utils/operators.js';
-
-
-const p1 = new Promise((resolve, reject) => setTimeout(() => resolve('Promise 1 terminou'), 3000))
-
-const p2 = new Promise((resolve, reject) => setTimeout(() => reject('Cancelado'), 1000))
-
-Promise.race([p1, p2])
-  .then(console.log)
-  .catch(console.log)
 
 const operations = pipe(
   partialize(takeUntil, 3),
   partialize(debounceTime, 500),
 )
 
-const action = operations(() => 
-  service
-    .sumItems("2143")
+const action = operations(() =>
+  timeoutPromise(200, service.sumItems("2143"))  
     .then(console.log)
     .catch(console.log)
 )
