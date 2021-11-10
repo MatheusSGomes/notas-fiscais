e802bec09bcb7e16bb607b383b66301b6c696bb4
Matheus Gomes <math.s.gomes@gmail.com>
2021-11-10 11:10:11 -0300

    função delay criada para se usada no mecanismo de retry

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-import { handleStatus, log, timeoutPromise } from './utils/promise-helpers.js';
+import { handleStatus, log, timeoutPromise, delay } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe, compose } from './utils/operators.js';
@@ -9,7 +9,7 @@ const operations = pipe(
 )
 
 const action = operations(() =>
-  timeoutPromise(200, service.sumItems("2143"))  
+  timeoutPromise(200, service.sumItems("2143"))
     .then(console.log)
     .catch(console.log)
 )
